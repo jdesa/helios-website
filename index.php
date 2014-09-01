e6c52168eb8665c690ac8d57aa771ca83d5e7046
Jacqui De Sa <jdesa@mit.edu>
2014-09-01 01:19:24 -0400

    Fixing accidental copy error in php file

--- index.php
+++ index.php
@@ -35,52 +35,6 @@
                   <li class="active" class="test"><a href="/index.html";>Home</a></li>
                   <li class="active" class="test"><a href="/about.html";>About</a></li>
                   <li class="active" class="test"><a href="/media.html";>Watch</a></li>
-                <li class="active" class="test"><a href="#">Internal</a>                    </li>
-              </ul>
-             <h3 class="masthead-brand" style="display: inline-block">The Helios Ensemble</h3>
-            </div>
-          </div>
-          </div>
-
-            
-                    <h1 class="cover-heading">The Helios Ensemble</h1>
-            <p class="lead"></p><!DOCTYPE html>
-<html lang="en">
-  <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" type="image/png" href="/centeredsun.png">
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'><link href='http://fonts.googleapis.com/css?family=Bilbo+Swash+Caps' rel='stylesheet' type='text/css'>s    <title>The Helios Ensemble</title>
-
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
-    <!-- Custom styles for this template -->
-    <link href="styling.css" rel="stylesheet"> 
-      
-  </head> 
-    <!-- Starting the body -->
-<!--- Navbar -->
-  <body>
-    <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="cover-container">
-          <div class="masthead clearfix">
-              <div class="inner">
-              <ul class="nav masthead-nav">
-                  <li class="active" class="test"><a href="/index.html";>Home</a></li>
-                  <li class="active" class="test"><a href="/about.html";>About</a></li>
-                  <li class="active" class="test"><a href="/media.html";>Watch</a></li>
                 <li class="active" class="test"><a href="#">Upcoming</a>                    </li>
               </ul>
              <h3 class="masthead-brand" style="display: inline-block">The Helios Ensemble</h3>
@@ -94,16 +48,6 @@
             <p class="lead">
              The Helios Ensemble is a 42-person choir ensemble based in Tucson, AZ, lead by Ben Hansen....next concert...etc?
             </p>
-          </div>
-        </div>
-      </div>
-      </div>
-<!-- Page 2: Jumbotron with Helios Name and Info about the  -->
-          
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-      </body>
-      </html>
           </div>
         </div>
       </div>
